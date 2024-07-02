b6562e5f099f29b63c2d932d01bc5e375b94ad45
Thiago <thiagohattenhauer16@gmail.com>
2024-07-01 21:57:48 -0300

    Sign-in

--- signin.php
+++ signin.php
@@ -1,10 +1,18 @@
+<?php
+    if(!isset($_SESSION)) 
+    { 
+        session_start(); 
+    } 
+    $logado = isset($_SESSION['email']);
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <title>Cadastre-se</title>
+    <title>Novo Usuário</title>
 </head>
+<?php if(!$logado){ echo '
 <h1>Cadastre-se</h1>
 <form action="salvar-usuario.php" method="POST">
     <input type="hidden" name="acao" value="cadastrar">
@@ -26,5 +34,34 @@
     </div>
     <div class="mb-3">
         <button type="submit" class="btn btn-primary">Enviar</button>
-    </div>    
+    </div>
+    ';} ?>
+    <?php if($logado){ echo '
+    <h1>Cadastrar novo usuário</h1>
+    <form action="salvar-usuario.php" method="POST">
+    <input type="hidden" name="acao" value="cadastrar">
+    <div class="mb-3">
+        <label>Cargo</label>
+        <input type="number"value="1" min="1" max="3" step="1" name="cargo" class="form-control">
+    </div
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Apelido</label>
+        <input type="text" name="apelido" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>E-mail</label>
+        <input type="email" name="email" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="senha" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+    ';} ?>
 </form>
